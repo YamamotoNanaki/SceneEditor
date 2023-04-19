961d9dfc6221b3bd50472c46c5c1a65221a7a854
LC1A_21_Yamamoto_Nanaki <k021g1320@g.neec.ac.jp>
2023-04-19 17:05:57 +0900

    なんかバグる

--- Data/Shaders/PostEffectPS.hlsl
+++ Data/Shaders/PostEffectPS.hlsl
@@ -1,15 +1,26 @@
 #include "PostEffect.hlsli"
 
-Texture2D<float4> tex : register(t0); // 0番スロットに設定されたテクスチャ
+Texture2D<float4> tex0 : register(t0); // 0番スロットに設定されたテクスチャ
+Texture2D<float4> tex1 : register(t1); // 1番スロットに設定されたテクスチャ
 SamplerState smp : register(s0); // 0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
     float s = shift;
-    float4 texcolor = tex.Sample(smp, input.uv);
-    texcolor.r = tex.Sample(smp, input.uv + float2(-s, s)).r;
-    texcolor.g = tex.Sample(smp, input.uv + float2(0, -s)).g;
-    texcolor.b = tex.Sample(smp, input.uv + float2(s, 0)).b;
+    float4 texcolor = tex0.Sample(smp, input.uv);
+    float4 texcolor1 = tex1.Sample(smp, input.uv);
+    //texcolor.r = tex0.Sample(smp, input.uv + float2(-s, s)).r;
+    //texcolor.g = tex0.Sample(smp, input.uv + float2(0, -s)).g;
+    //texcolor.b = tex0.Sample(smp, input.uv + float2(s, 0)).b;
+    //texcolor1.r = tex1.Sample(smp, input.uv + float2(0, -s)).r;
+    //texcolor1.g = tex1.Sample(smp, input.uv + float2(s, 0)).g;
+    //texcolor1.b = tex1.Sample(smp, input.uv + float2(s, -s)).b;
     
-    return texcolor;
+    float4 color = texcolor;
+    if (fmod(input.uv.y, 0.1f) < 0.05f)
+    {
+        color = texcolor1;
+    }
+    
+    return color;
 }
--- Engine/2d/PostEffect.cpp
+++ Engine/2d/PostEffect.cpp
@@ -1,7 +1,7 @@
 #include "PostEffect.h"
 #include "Window.h"
 #include "DirectX12.h"
-#include "Input.h"
+//#include "Input.h"
 #include <cassert>
 #include <d3dx12.h>
 #include <d3dcompiler.h>
@@ -34,20 +34,20 @@ void PostEffect::Draw(bool add)
 	cmdList->SetGraphicsRootSignature(rootSignature.Get());
 	cmdList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
 
-	if (Input::Instance()->KeyTriggere(KEY::num0))
-	{
-		static int tex = 0;
-		tex = (tex + 1) % 2;
+	//if (Input::Instance()->KeyTriggere(KEY::num0))
+	//{
+	//	static int tex = 0;
+	//	tex = (tex + 1) % 2;
 
-		D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
-		srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-		srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-		srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
-		srvDesc.Texture2D.MipLevels = 1;
+	//	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
+	//	srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	//	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	//	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+	//	srvDesc.Texture2D.MipLevels = 1;
 
-		ID3D12Device* device = DirectX12::Instance()->GetDevice();
-		device->CreateShaderResourceView(texBuff[tex].Get(), &srvDesc, descHeapSRV->GetCPUDescriptorHandleForHeapStart());
-	}
+	ID3D12Device* device = DirectX12::Instance()->GetDevice();
+	//	device->CreateShaderResourceView(texBuff[tex].Get(), &srvDesc, descHeapSRV->GetCPUDescriptorHandleForHeapStart());
+	//}
 	// 頂点バッファの設定
 	cmdList->IASetVertexBuffers(0, 1, &vi->GetVertexView());
 
@@ -57,7 +57,8 @@ void PostEffect::Draw(bool add)
 	// 定数バッファビューをセット
 	cmdList->SetGraphicsRootConstantBufferView(0, this->constBuffTransform->GetGPUVirtualAddress());
 	// シェーダリソースビューをセット
-	cmdList->SetGraphicsRootDescriptorTable(1, descHeapSRV->GetGPUDescriptorHandleForHeapStart());
+	cmdList->SetGraphicsRootDescriptorTable(1, CD3DX12_GPU_DESCRIPTOR_HANDLE(descHeapSRV->GetGPUDescriptorHandleForHeapStart(), 0, device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV)));
+	cmdList->SetGraphicsRootDescriptorTable(2, CD3DX12_GPU_DESCRIPTOR_HANDLE(descHeapSRV->GetGPUDescriptorHandleForHeapStart(), 1, device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV)));
 	cmdList->SetGraphicsRootConstantBufferView(2, constBuffPostEffect.Get()->GetGPUVirtualAddress());
 	// 描画コマンド
 	cmdList->DrawInstanced(4, 1, 0, 0);
@@ -133,7 +134,7 @@ void PostEffect::Initialize()
 	D3D12_DESCRIPTOR_HEAP_DESC srvDescHeapDesc = {};
 	srvDescHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
 	srvDescHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
-	srvDescHeapDesc.NumDescriptors = 1;
+	srvDescHeapDesc.NumDescriptors = 2;
 
 	result = device->CreateDescriptorHeap(&srvDescHeapDesc, IID_PPV_ARGS(&descHeapSRV));
 	assert(SUCCEEDED(result));
@@ -144,8 +145,11 @@ void PostEffect::Initialize()
 	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
 	srvDesc.Texture2D.MipLevels = 1;
 
-	device->CreateShaderResourceView(texBuff[0].Get(), &srvDesc, descHeapSRV->GetCPUDescriptorHandleForHeapStart());
-
+	for (int i = 0; i < 2; i++)
+	{
+		device->CreateShaderResourceView(texBuff[i].Get(), &srvDesc, CD3DX12_CPU_DESCRIPTOR_HANDLE(
+			descHeapSRV->GetCPUDescriptorHandleForHeapStart(), i, device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV)));
+	}
 	D3D12_DESCRIPTOR_HEAP_DESC rtvDescHeapDesc{};
 	rtvDescHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
 	rtvDescHeapDesc.NumDescriptors = 2;
@@ -382,13 +386,16 @@ void PostEffect::CreateGraphicsPipelineState()
 	gpipeline.SampleDesc.Count = 1; // 1ピクセルにつき1回サンプリング
 
 	// デスクリプタレンジ
-	CD3DX12_DESCRIPTOR_RANGE descRangeSRV;
-	descRangeSRV.Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0); // t0 レジスタ
+	CD3DX12_DESCRIPTOR_RANGE descRangeSRV0;
+	descRangeSRV0.Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0); // t0 レジスタ
+	CD3DX12_DESCRIPTOR_RANGE descRangeSRV1;
+	descRangeSRV1.Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 1); // t1 レジスタ
 
 	// ルートパラメータ
-	CD3DX12_ROOT_PARAMETER rootparams[2];
+	CD3DX12_ROOT_PARAMETER rootparams[3];
 	rootparams[0].InitAsConstantBufferView(0, 0, D3D12_SHADER_VISIBILITY_ALL);
-	rootparams[1].InitAsDescriptorTable(1, &descRangeSRV, D3D12_SHADER_VISIBILITY_ALL);
+	rootparams[1].InitAsDescriptorTable(1, &descRangeSRV0, D3D12_SHADER_VISIBILITY_ALL);
+	rootparams[2].InitAsDescriptorTable(1, &descRangeSRV1, D3D12_SHADER_VISIBILITY_ALL);
 
 	// スタティックサンプラー
 	CD3DX12_STATIC_SAMPLER_DESC samplerDesc = CD3DX12_STATIC_SAMPLER_DESC(0, D3D12_FILTER_MIN_MAG_MIP_POINT); // s0 レジスタ
