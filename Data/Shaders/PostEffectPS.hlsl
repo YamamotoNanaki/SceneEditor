b263470ec41c12af73bd10dfbd2c0284f9701897
LC1A_21_Yamamoto_Nanaki <k021g1320@g.neec.ac.jp>
2023-04-27 11:30:40 +0900

    ちょっと変更

--- Data/Shaders/PostEffectPS.hlsl
+++ Data/Shaders/PostEffectPS.hlsl
@@ -4,7 +4,7 @@ Texture2D<float4> tex0 : register(t0); // 0番スロットに設定されたテクスチャ
 Texture2D<float4> tex1 : register(t1); // 1番スロットに設定されたテクスチャ
 SamplerState smp : register(s0); // 0番スロットに設定されたサンプラー
 
-float4 Blur1Pixel(float2 uv)
+float4 Blur1Pixel(float2 uv, Texture2D<float4> tex)
 {
     float4 col = float4(0, 0, 0, 1);
     float u = 1.f / 1280.f;
@@ -14,7 +14,7 @@ float4 Blur1Pixel(float2 uv)
     {
         for (int y = 0; y < 3; y++)
         {
-            col += tex1.Sample(smp, uv + float2(u * (x - 1), v * (y - 1)));
+            col += tex.Sample(smp, uv + float2(u * (x - 1), v * (y - 1)));
         }
     }
     
@@ -28,7 +28,7 @@ float Gaussian(float2 drawuv, float2 pickuv, float sigma)
     return exp(-(d * d) / (2 * sigma * sigma));
 }
 
-float4 GaussianBlur(float2 uv)
+float4 GaussianBlur(float2 uv, Texture2D<float4> tex)
 {
     float totalWeight = 0, _sigma = 0.005, _stepWidth = 0.001;
     float4 col = float4(0, 0, 0, 1);
@@ -39,7 +39,7 @@ float4 GaussianBlur(float2 uv)
         {
             float2 pickuv = uv + float2(px, py);
             float weight = Gaussian(uv, pickuv, _sigma);
-            col += tex0.Sample(smp, pickuv) * weight;
+            col += tex.Sample(smp, pickuv) * weight;
             totalWeight += weight;
         }
     }
@@ -56,19 +56,13 @@ float4 main(VSOutput input) : SV_TARGET
     texcolor.r = tex0.Sample(smp, input.uv + float2(-s, s)).r;
     texcolor.g = tex0.Sample(smp, input.uv + float2(0, -s)).g;
     texcolor.b = tex0.Sample(smp, input.uv + float2(s, 0)).b;
-    if(gray)
-    {
-        float gray = texcolor.r * 0.299 + texcolor.g * 0.587 + texcolor.b * 0.114;
-
-        texcolor = float4(gray + sepia, gray, gray - sepia, texcolor.a);
-    }
     float4 texcolor1;
     
     color = texcolor;
     if (kadai)
     {
         texcolor.rgb = 1 - texcolor.rgb;
-        texcolor1 = Blur1Pixel(input.uv);
+        texcolor1 = Blur1Pixel(input.uv, tex1);
         float num = 0;
     
         color = texcolor;
@@ -79,7 +73,13 @@ float4 main(VSOutput input) : SV_TARGET
     }
     if (gaussianBlur)
     {
-        color = GaussianBlur(input.uv);
+        color = GaussianBlur(input.uv, tex0);
+    }
+    if (gray)
+    {
+        float gray = color.r * 0.299 + color.g * 0.587 + color.b * 0.114;
+
+        color = float4(gray + sepia, gray, gray - sepia, color.a);
     }
     return color;
 }
